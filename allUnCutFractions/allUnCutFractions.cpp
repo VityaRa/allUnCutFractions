618200236b7f951d1155beae454a40cb8bcbd406
VityaRa <blackops2in@mail.ru>
2021-02-28 00:35:52 +0300

    proj deployed

---
+++ allUnCutFractions/allUnCutFractions.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int findDivisor (int numerator, int denominator) {
+    while (numerator != denominator) {
+        if (numerator > denominator) numerator -= denominator;
+        else denominator -= numerator;
+    }
+    return numerator;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 1; i < N; i++) {
+        if (findDivisor(i, N) == 1)
+            cout << i << "/" << N << endl;
+    }
+    cin >> N;
+}
+
+
